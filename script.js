09dd006d7f90d2884af72593a3f1cb6f473fd04d
codeldu <huseynagha.hagverdiyev@code.edu.az>
2023-11-24 13:18:29 +0400

    init commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// const restoran = {
+//     name : 'Yashil ada',
+//     location : '3cu dalan, Baki',
+//     categories : ['Milli', 'Gurcu', 'Italyan', 'Vegetarian'],
+//     starterMenu : ['Coban salati', 'Manqal salati', 'Suzme', 'Sezar', 'Turshu'],
+//     mainMenu : ['Kabab', 'Pizza', 'Xengel', 'Dushbere', 'Pasta'],
+//     order : function(starterInd, mainIndex){
+//         return [ this.starterMenu[starterInd], this. mainMenu[mainIndex] ]
+//     }
+// }
+
+
+
+// let [first, ,second] = restoran.categories;
+
+// console.log(first,second);
+
+// let temp = first;
+// first = second;
+// second = temp;
+
+// [second, first] = [first,second]
+
+// console.log(first,second)
+
+
+// console.log(restoran.order(1,3));
+
+// let [meze, yemek] = restoran.order(1,3);
+
+
+// let nestedArr = [2, 4, [6 ,[ 8, 10]]];
+
+// let [a,b,[c,[d,e]]] = nestedArr;
+
+// console.log(a,b,c,d,e);
+
+
+// let myArr = [2];
+
+// let [a='Error',b='Error',c='Error'] = myArr;
+
+// console.log(a,b,c);
