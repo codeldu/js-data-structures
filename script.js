5281ec87bb86a4a9ed568304eb50a6ba13e30e25
codeldu <huseynagha.hagverdiyev@code.edu.az>
2023-11-27 18:13:03 +0400

    Salam

--- script.js
+++ script.js
@@ -1,13 +1,100 @@
-// const restoran = {
-//     name : 'Yashil ada',
-//     location : '3cu dalan, Baki',
-//     categories : ['Milli', 'Gurcu', 'Italyan', 'Vegetarian'],
-//     starterMenu : ['Coban salati', 'Manqal salati', 'Suzme', 'Sezar', 'Turshu'],
-//     mainMenu : ['Kabab', 'Pizza', 'Xengel', 'Dushbere', 'Pasta'],
-//     order : function(starterInd, mainIndex){
-//         return [ this.starterMenu[starterInd], this. mainMenu[mainIndex] ]
-//     }
+const restoran = {
+    name : 'Yashil ada',
+    newLocation : '3cu dalan, Baki',
+    categories : ['Milli', 'Gurcu', 'Italyan', 'Vegetarian'],
+    starterMenu : ['Coban salati', 'Manqal salati', 'Suzme', 'Sezar', 'Turshu'],
+    mainMenu : ['Kabab', 'Pizza', 'Xengel', 'Dushbere', 'Pasta'],
+    openHours : {
+        mon : {
+            open : '10:00',
+            close : '20:00'
+        },
+        tue : {
+            open : '10:00',
+            close : '22:00'
+        },
+        wed : {
+            open : '10:00',
+            close : '23:00'
+        }
+    },
+    order : function(starterInd, mainIndex){
+        return [ this.starterMenu[starterInd], this. mainMenu[mainIndex] ]
+    },
+    delivery : function({customerName, orders, hour = "bilinmeyen"}){
+        console.log(`${customerName} saat ${hour}-da ${orders} sifarish verdi`);
+    },
+    cook : function(mehsul1, mehsul2, mehsul3){
+        console.log(`Menim pizzam ${mehsul1}, ${mehsul2} ve ${mehsul3} terkiblidir`);
+    }
+}
+
+
+
+let mehsullar = ['Pendir', 'Pomidor','Sosis'];
+
+
+restoran.cook(...mehsullar);
+
+// console.log(mehsullar);
+
+// let newMenu = [ "Levengi", ...restoran.mainMenu];
+// restoran.mainMenu = newMenu;
+
+// let allMenu = [...restoran.mainMenu, ...restoran.starterMenu];
+// console.log(allMenu);
+
+// let myName = "Huseynaga";
+
+// let letters = [...myName];
+
+// console.log(letters);
+
+// restoran.mainMenu.unshift("Levengi");
+
+// console.log(restoran);
+
+// console.log(newMenu);
+
+// let sifarish = {
+//     customerName : "Qeribeli",
+//     orders : "Pizza",
+//     // hour : "14:00"
 // }
+
+// restoran.delivery(sifarish);
+
+
+// let {openHours} = restoran;
+// console.log(openHours);
+
+// let {openHours: {mon : {open : achiq, close: bagli}}} = restoran;
+
+
+// console.log(achiq, bagli);
+
+
+
+// let { name : ad = "Ad teyin edilmeyib", newLocation : loc = "Teyin edilmeyib", menu : mnu = "Menu teyin edilmeyib"} = restoran;
+
+// console.log(ad , loc , mnu);
+
+// let { name : ad , newLocation : konum} = restoran;
+
+// let a = 5;
+// let b = 8;
+
+// let obj = {a: 22, b : 10};
+
+// // ({a , b} = obj);
+
+// let { a : first, b : second} = obj;
+
+
+// console.log(first,second);
+
+
+
 
 
 
@@ -41,3 +128,47 @@
 // let [a='Error',b='Error',c='Error'] = myArr;
 
 // console.log(a,b,c);
+
+
+// Spread operators (...)
+
+// let arr = [5,6,7,8];
+
+
+// let newArray = [1,2,3,4,...arr];
+
+// console.log(newArray);
+
+// let obj = {
+//     name : "Ayxan"
+// }
+
+// let secObj = {...obj};
+
+// secObj.name = "Muslum";
+
+// console.log(obj);
+// console.log(secObj);
+// console.log(secObj);
+
+// let newObj = {
+//     ...obj,
+//     age: 20
+// }
+
+// console.log(newObj);
+
+
+
+// let numArray = [1,2,4,6,7,9];
+
+// let sum = 0;
+
+// for (const x in numArray) {
+//     sum += numArray[x];
+// }
+
+// console.log(sum);
+
+
+console.log(sum(1,2));
